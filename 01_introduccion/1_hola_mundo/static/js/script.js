8d56356b9b928e9feafebfc930c92ebfbe65c0e7
axelescobar-art <axelescobar@liceovvh.cl>
2026-04-13 08:31:42 -0400

    ok

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// Prueba de convencion con js//
+console.log("hola mundo, desde archivo externo!...");
+
+// 1) Hola mundo con variable y cost.
+const saludo ="Hola mundo, ";
+let destinatario = "estudiantes"
+// Impresion de saludo de saludo concatenando con (+)
+console.log(saludo + "para los" + destinatario);
+
+// 2) Saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre su nombre: ");
+//Imprimir un nombre ingresado con un prompt
+console.log("hola, " + nombre);
---
+++ 02_tipos_de_datos/1-Tipo_texto/static/js/script.js
@@ -0,0 +1,1 @@
+console.log("conexion con js correcta...")
