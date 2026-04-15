3d291aaf9991c1fee86f6aa902f3ea19f264acd6
axelescobar-art <axelescobar@liceovvh.cl>
2026-04-15 08:12:34 -0400

    ok

--- 02_tipos_de_datos/1-Tipo_texto/static/js/script.js
+++ 02_tipos_de_datos/1-Tipo_texto/static/js/script.js
@@ -1,1 +1,9 @@
-console.log("conexion con js correcta...")
+console.log("conexion con js correcta...");
+/*
+Concatenacion de texto con el signo + 
+podemos abrir textos variables
+*/
+const nombre = "Axel";
+const apellido = "Escobar";
+//Unimos ambas constantes con un texto extra.
+console.lug("hola, mi nombre es:" + nombre + " " + apellido)
