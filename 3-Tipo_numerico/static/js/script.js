935bfcaea05e6658596e9b4a73ec58f3be7c4148
axelescobar-art <axelescobar@liceovvh.cl>
2026-04-20 08:28:25 -0400

    ok

---
+++ 3-Tipo_numerico/static/js/script.js
@@ -0,0 +1,37 @@
+console.log("Conexion exitosa son js...");
+
+//Tipos de datos numericos
+
+//1.- Declaracion de numero (creacion de variables numericas)
+let edad = 31;
+let temperatura= 5;
+let precio= 19990;
+let pi= 3.1416;
+let descuento= 0.25;
+
+//Operaciones basicas
+let suma= 10 + 5; //15
+let resta= 10 - 5; //5
+let multiplicacion= 10 * 5; //50
+let division= 10/2; //5
+let modulo= 10 % 3; //1
+let potencia= 2**3 //8
+
+console.log(`El resultado de 2 elevado a 3 en potencia es: ${2**3}.`)
+
+//3.- Incrementando y descrementando (suma(++) o resta (--)
+
+//4,- Numeros con decimales
+let promedio= 6.5;
+let altura= 1.67;
+let peso= 90.3;
+
+//5.- Numeros grandes y notacion cientifico 
+let poblacion= 1e6;
+console.log("Numero grande 1e6:" + poblacion);
+let numeroPequeno= 5e-3;
+console.log("Numero pequeño 5e-3:" + total);
+
+//6.- Operaciones combinadas
+//calculo de precios
+let precioProducto=1000;
